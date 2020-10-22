07876b54f4e2c4ea419e2e036db90130c5d99782
anders <anders@stormer.dk>
2020-10-22 15:09:59 +0200

    add start 2

---
+++ src/Components/App.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import HomePage from "./HomePage/HomePage";
+
+
+import "tabler-react/dist/Tabler.css";
+
+function App() {
+  return (
+    <Router>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          {/* <Route exact path="/400" component={Error400} />
+          <Route exact path="/401" component={Error401} />
+          <Route exact path="/403" component={Error403} />
+          <Route exact path="/404" component={Error404} />
+          <Route exact path="/500" component={Error500} />
+          <Route exact path="/503" component={Error503} />
+          <Route exact path="/blog" component={BlogPage} />
+          <Route exact path="/cards" component={CardsDesignPage} />
+          <Route exact path="/charts" component={ChartsPage} />
+          <Route exact path="/email" component={Email} />
+          <Route exact path="/empty-page" component={Empty} />
+          <Route exact path="/form-elements" component={FormElementsPage} />
+          <Route exact path="/forgot-password" component={ForgotPasswordPage} />
+          <Route exact path="/gallery" component={GalleryPage} />
+          <Route exact path="/icons" component={IconPage} />
+          <Route exact path="/login" component={LoginPage} />
+          <Route exact path="/maps" component={MapCardsPage} />
+          <Route exact path="/pricing-cards" component={PricingCardsPage} />
+          <Route exact path="/profile" component={ProfilePage} />
+          <Route exact path="/register" component={RegisterPage} />
+          <Route exact path="/store" component={StoreCardsPage} />
+          <Route component={Error404} /> */}
+        </Switch>
+      </Router>
+  );
+}
+
+export default App;
